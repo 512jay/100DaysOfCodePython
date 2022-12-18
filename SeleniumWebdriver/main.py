9333a6f2c15f3006726da16dd07fca96cd06264e
John Davis <512jay@gmail.com>
2022-12-18 14:52:11 -0500

    Update with completed project 47

---
+++ SeleniumWebdriver/main.py
@@ -0,0 +1,15 @@
+import time
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+
+service = Service('/Development/chromedriver')
+
+service.start()
+
+driver = webdriver.Remote(service.service_url)
+
+driver.get('http://www.google.com/');
+
+time.sleep(5) # Let the user actually see something!
+
+driver.quit()
