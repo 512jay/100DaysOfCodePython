67c5367b2cd455c0c058d26e0c29dda4ccff41a8
John Davis <512jay@gmail.com>
2022-12-24 15:11:01 -0500

    Log into linked in with email and password in env

--- Automating Job Application on LinkeIn/main.py
+++ Automating Job Application on LinkeIn/main.py
@@ -2,18 +2,31 @@ from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 import time
+import os
+email = os.environ.get('email')
+password = os.environ.get('password')
 
 service = Service('/Development/chromedriver')
 
 service.start()
 
-url = "https://www.linkedin.com/jobs/search/?currentJobId=3349997861&f_AL=true&f_E=2&f_WT=2&keywords=python%20developer%20&refresh=true&sortBy=R"
+url = "https://www.linkedin.com/jobs/search/?currentJobId=3349997861&f_AL=true&f_E=2&f_WT=2&keywords=python%20developer%20&refresh=true&sortBy=R "
 
 
 driver = webdriver.Remote(service.service_url)
 driver.get(url)
-time.sleep(5)
+time.sleep(3)
 sign_in = driver.find_element(by=By.LINK_TEXT, value="Sign in")
 sign_in.click()
+
+username_input = driver.find_element(by=By.ID, value="username")
+username_input.send_keys(email)
+
+password_input = driver.find_element(by=By.ID, value="password")
+password_input.send_keys(password)
+
+sign_in_button = driver.find_element(by=By.CLASS_NAME, value="btn__primary--large")
+sign_in_button.click()
+
 time.sleep(30)
 driver.quit()
