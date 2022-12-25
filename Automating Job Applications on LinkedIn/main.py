d395c8a8a6ffc65c39538a7a8e219e5f0f73fcb2
John Davis <512jay@gmail.com>
2022-12-25 00:44:41 -0500

    Click to save job and follow company

--- Automating Job Applications on LinkedIn/main.py
+++ Automating Job Applications on LinkedIn/main.py
@@ -2,6 +2,7 @@ from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import WebDriverWait
 import time
 import os
 email = os.environ.get('email')
@@ -11,9 +12,7 @@ service = Service('/Development/chromedriver')
 
 service.start()
 
-url = "https://www.linkedin.com/jobs/search/?currentJobId=3349997861&f_AL=true&f_E=2&f_WT=2&keywords=python" \
-      "%20developer%20&refresh=true&sortBy=R "
-
+url = "https://www.linkedin.com/jobs/search/?currentJobId=3283361601&f_AL=true&f_BE=3&f_E=2&f_WT=2&keywords=python%20developer%20&refresh=true&sortBy=R"
 
 driver = webdriver.Remote(service.service_url)
 driver.get(url)
@@ -29,8 +28,19 @@ password_input.send_keys(password)
 
 password_input.send_keys(Keys.ENTER)
 
-easy_apply_button = driver.find_element(by=By.CLASS_NAME, value="jobs-apply-button")
-easy_apply_button.click()
+next_btn = driver.find_element(by=By.CLASS_NAME, value="jobs-save-button")
+next_btn.click()
 
-time.sleep(30)
+#  Scroll down to the bottom of the page
+next_btn.send_keys(Keys.END)
+
+# Close messages overlay
+message_overlay = driver.find_element(by=By.ID, value="ember114")
+message_overlay.click()
+
+follow_button = driver.find_element(by=By.CLASS_NAME, value="follow")
+follow_button.click()
+
+
+time.sleep(10)
 driver.quit()
