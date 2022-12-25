a0ad9ab674789b80f48b852e9ec680330db6afb2
John Davis <512jay@gmail.com>
2022-12-25 01:33:57 -0500

    Find all the jobs on the page and print out the details

--- Automating Job Applications on LinkedIn/main.py
+++ Automating Job Applications on LinkedIn/main.py
@@ -2,7 +2,6 @@ from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.support.ui import WebDriverWait
 import time
 import os
 email = os.environ.get('email')
@@ -28,19 +27,30 @@ password_input.send_keys(password)
 
 password_input.send_keys(Keys.ENTER)
 
-next_btn = driver.find_element(by=By.CLASS_NAME, value="jobs-save-button")
-next_btn.click()
-
-#  Scroll down to the bottom of the page
-next_btn.send_keys(Keys.END)
-
 # Close messages overlay
 message_overlay = driver.find_element(by=By.ID, value="ember114")
 message_overlay.click()
 
-follow_button = driver.find_element(by=By.CLASS_NAME, value="follow")
-follow_button.click()
+result_text = driver.find_element(by=By.CLASS_NAME, value="jobs-search-results-list__title-heading")
+result = result_text.text.split("\n")
+number_of_jobs = int(result[1].split()[0])
+print(number_of_jobs)
+
+jobs = driver.find_elements(by=By.CLASS_NAME, value="job-card-container")
+for job in jobs:
+    print(job.text)
+
+# next_btn = driver.find_element(by=By.CLASS_NAME, value="jobs-save-button")
+# next_btn.click()
+#
+# #  Scroll down to the bottom of the page
+# next_btn.send_keys(Keys.END)
+#
+
+#
+# follow_button = driver.find_element(by=By.CLASS_NAME, value="follow")
+# follow_button.click()
 
 
-time.sleep(10)
+time.sleep(5000)
 driver.quit()
