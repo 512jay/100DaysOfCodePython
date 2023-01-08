241498bf4fbf10871246ab1d6d29a7838113dcbc
John Davis <512jay@gmail.com>
2023-01-07 22:29:22 -0500

    Connect with selenium from a class

--- Internet Speed Twitter Complaint Bot/complaint_bot.py
+++ Internet Speed Twitter Complaint Bot/complaint_bot.py
@@ -1,10 +1,29 @@
 import os
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+import time
+
 PROMISED_DOWN = 200
 PROMISED_UP = 100
 CHROME_DRIVER_PATH = "C:\\Development\\chromedriver.exe"
 TWITTER_EMAIL = os.environ.get("EMAIL")
 TWITTER_PASSWORD = os.environ.get("PASSWORD")
 
-print(CHROME_DRIVER_PATH)
-print(TWITTER_EMAIL)
-print(TWITTER_PASSWORD)
+
+class InternetSpeedTwitterBot:
+    """A class for connecting to selenium"""
+    def __init__(self):
+        self.service = Service('/Development/chromedriver')
+        self.service.start()
+        url = "https://twitter.com"
+        self.driver = webdriver.Remote(self.service.service_url)
+        self.driver.get(url)
+        time.sleep(5)
+
+    def quit(self):
+        self.driver.quit()
+
+
+complaint_bot = InternetSpeedTwitterBot()
+complaint_bot.quit()
