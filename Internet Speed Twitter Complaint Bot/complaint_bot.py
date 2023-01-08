3fee2386088c4d8b5d098b2669ce4dc63025e98f
John Davis <512jay@gmail.com>
2023-01-08 18:43:54 -0500

    Bypass initial twitter login screen

--- Internet Speed Twitter Complaint Bot/complaint_bot.py
+++ Internet Speed Twitter Complaint Bot/complaint_bot.py
@@ -14,16 +14,41 @@ TWITTER_PASSWORD = os.environ.get("PASSWORD")
 class InternetSpeedTwitterBot:
     """A class for connecting to selenium"""
     def __init__(self):
+        self.down = None
+        self.up = None
         self.service = Service('/Development/chromedriver')
         self.service.start()
-        url = "https://twitter.com"
         self.driver = webdriver.Remote(self.service.service_url)
+        self.driver.maximize_window()
+
+    def get_internet_speed(self):
+        url = "https://www.speedtest.net/"
+        # class start-text
         self.driver.get(url)
-        time.sleep(5)
+        start_test = self.driver.find_element(by=By.CLASS_NAME, value="start-text")
+        time.sleep(15)
+        start_test.click()
+        time.sleep(40)
+        down = self.driver.find_element(by=By.CLASS_NAME, value="download-speed").text
+        up = self.driver.find_element(by=By.CLASS_NAME, value="upload-speed").text
+        print(down)
+        print(up)
+
+    def tweet_at_provider(self):
+        url = "https://twitter.com/i/flow/login"
+        self.driver.get(url)
+        # login = self.driver.find_element(by=By.LINK_TEXT, value="Log in")
+        # login.click()
+        time.sleep(10)
+        # google = self.driver.find_element(by=By.CSS_SELECTOR, value="title=Sign in with Google Button")
+        # google.click()
+        # time.sleep(60)
 
     def quit(self):
         self.driver.quit()
 
 
 complaint_bot = InternetSpeedTwitterBot()
+# complaint_bot.get_internet_speed()
+complaint_bot.tweet_at_provider()
 complaint_bot.quit()
