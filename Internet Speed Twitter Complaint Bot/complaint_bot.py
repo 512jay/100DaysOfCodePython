1c3d1f458db93d0edac591c7e2227af1e8e3a2ee
John Davis <512jay@gmail.com>
2023-01-09 11:20:08 -0500

    Login to twitter

--- Internet Speed Twitter Complaint Bot/complaint_bot.py
+++ Internet Speed Twitter Complaint Bot/complaint_bot.py
@@ -10,12 +10,14 @@ PROMISED_UP = 100
 CHROME_DRIVER_PATH = "C:\\Development\\chromedriver.exe"
 TWITTER_EMAIL = os.environ.get("EMAIL")
 TWITTER_PASSWORD = os.environ.get("PASSWORD")
+TWITTER_HANDLE = os.environ.get("TWITTER_HANDLE")
+
 
 class InternetSpeedTwitterBot:
     """A class for connecting to selenium"""
     def __init__(self):
-        self.down = None
-        self.up = None
+        self.down = 0
+        self.up = 0
         self.service = Service('/Development/chromedriver')
         self.service.start()
         self.driver = webdriver.Remote(self.service.service_url)
@@ -51,7 +53,7 @@ class InternetSpeedTwitterBot:
         input_email.send_keys(TWITTER_EMAIL, Keys.ENTER)
         time.sleep(5)
         twitter_name = self.driver.find_element(by=By.TAG_NAME, value="input")
-        twitter_name.send_keys("DevCroesus", Keys.ENTER)
+        twitter_name.send_keys(os.environ.get("TWITTER_HANDLE"), Keys.ENTER)
         time.sleep(5)
         password = self.driver.find_elements(by=By.TAG_NAME, value="input")
         password[1].send_keys(TWITTER_PASSWORD, Keys.ENTER)
