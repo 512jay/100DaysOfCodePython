6e7b51792e4cc7bc0dd83bf3d05f701fd53c9fd4
John Davis <512jay@gmail.com>
2023-01-09 16:24:32 -0500

    Login to Instagram and clear popups

--- Instagram_Follow_Bot/follow_bot.py
+++ Instagram_Follow_Bot/follow_bot.py
@@ -1,13 +1,24 @@
 import os
+import time
 from selenium.webdriver.chrome.service import Service
 from selenium import webdriver
-import time
+from selenium.webdriver.common.by import By
+# from selenium.webdriver.common.keys import Keys
 
 
 CHROME_DRIVER_PATH = "/Development/chromedriver"
 SIMILAR_ACCOUNT = "thehightechrevolution"  # To be updated later
 USERNAME = os.environ.get("USERNAME")
 PASSWORD = os.environ.get("PASSWORD")
+
+
+def find_button_text_index(buttons, text) -> int:
+    index = 0
+    for button in buttons:
+        if button.text == text:
+            return index
+        else:
+            index += 1
 
 
 class InstaFollower:
@@ -20,10 +31,34 @@ class InstaFollower:
         self.service.start()
         self.driver = webdriver.Remote(self.service.service_url)
         self.driver.maximize_window()
-        time.sleep(5)
 
     def login(self):
-        pass
+        """Log in to Instagram and clear popups"""
+
+        url = "https://www.instagram.com/accounts/login/"
+        self.driver.get(url)
+        time.sleep(5)
+        # select inputs
+        inputs = self.driver.find_elements(by=By.TAG_NAME, value="input")
+        inputs[0].send_keys(USERNAME)
+        inputs[1].send_keys(PASSWORD)
+
+        # Click "Log in" button
+        time.sleep(5)
+        buttons = self.driver.find_elements(by=By.TAG_NAME, value="button")
+        buttons[find_button_text_index(buttons, "Log in")].click()
+
+        # Click "Not Now" button to saving login info
+        time.sleep(10)
+        buttons = self.driver.find_elements(by=By.TAG_NAME, value="button")
+        buttons[find_button_text_index(buttons, "Not Now")].click()
+
+        # Click "Not Now" to Notifications
+        time.sleep(5)
+        buttons = self.driver.find_elements(by=By.TAG_NAME, value="button")
+        buttons[find_button_text_index(buttons, "Not Now")].click()
+
+        time.sleep(30)
 
     def find_followers(self):
         pass
@@ -31,8 +66,12 @@ class InstaFollower:
     def follow(self):
         pass
 
+    def quit(self):
+        self.driver.quit()
+
 
 bot = InstaFollower(SIMILAR_ACCOUNT)
 bot.login()
 bot.find_followers()
 bot.follow()
+bot.quit()
