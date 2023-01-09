e2c8ceaecf0011ea8ce99dfa059dfe6b411db5fa
John Davis <512jay@gmail.com>
2023-01-09 14:29:02 -0500

    Add username, password and setup run configuration.

--- Instagram_Follow_Bot/follow_bot.py
+++ Instagram_Follow_Bot/follow_bot.py
@@ -1,2 +1,6 @@
+import os
+
 CHROME_DRIVER_PATH = "/Development/chromedriver"
 SIMILAR_ACCOUNT = "thehightechrevolution"  # To be updated later
+USERNAME = os.environ.get("USERNAME")
+PASSWORD = os.environ.get("PASSWORD")
