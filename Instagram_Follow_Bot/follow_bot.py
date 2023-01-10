7d72130ec7312a2b962f1d746ed3deec5908d3cc
John Davis <512jay@gmail.com>
2023-01-10 14:36:04 -0500

    Add random wait times

--- Instagram_Follow_Bot/follow_bot.py
+++ Instagram_Follow_Bot/follow_bot.py
@@ -1,15 +1,21 @@
 import os
 import time
+from random import choice
 from selenium.webdriver.chrome.service import Service
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.webdriver.common.action_chains import ActionChains
 # from selenium.webdriver.common.keys import Keys
-
 
 CHROME_DRIVER_PATH = "/Development/chromedriver"
 SIMILAR_ACCOUNT = "thehightechrevolution"  # To be updated later
 USERNAME = os.environ.get("USERNAME")
 PASSWORD = os.environ.get("PASSWORD")
+
+
+def wait():
+    random_times = [5, 6, 7, 8, 9]
+    time.sleep(choice(random_times))
 
 
 class InstaFollower:
@@ -30,6 +36,7 @@ class InstaFollower:
         for button in buttons:
             print(f"{index}: {button.text}")
             if text in button.text:
+                wait()
                 buttons[index].click()
                 break
             else:
@@ -40,31 +47,29 @@ class InstaFollower:
 
         url = "https://www.instagram.com/accounts/login/"
         self.driver.get(url)
-        time.sleep(5)
+        wait()
         # select inputs
         inputs = self.driver.find_elements(by=By.TAG_NAME, value="input")
         inputs[0].send_keys(USERNAME)
         inputs[1].send_keys(PASSWORD)
-
         # Click "Log in" button
-        time.sleep(5)
         self.click_tag_with_text("button", "Log in")
-
         # Click "Not Now" button to saving login info
-        time.sleep(10)
         self.click_tag_with_text("button", "Not Now")
-
         # Click "Not Now" to Notifications
-        time.sleep(5)
         self.click_tag_with_text("button", "Not Now")
-        time.sleep(5)
 
     def find_followers(self):
-        time.sleep(5)
+        wait()
         url = f"https://www.instagram.com/{SIMILAR_ACCOUNT}/"
         self.driver.get(url)
-        time.sleep(5)
+        wait()
         self.click_tag_with_text("li", "followers")
+        # scroll through followers.
+        # scrollable = self.driver.find_element(by=By.CSS_SELECTOR, value="div ._aano")
+        # delta_y = scrollable.rect['y']
+        # ActionChains(self.driver).scroll_by_amount(0, delta_y).perform()
+
         print("End of code!")
         time.sleep(600)
 
