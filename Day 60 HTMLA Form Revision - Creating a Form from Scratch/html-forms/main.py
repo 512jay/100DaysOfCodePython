8d5b233f77afc05be93553d02289e1b46e885888
John Davis <512jay@gmail.com>
2023-02-02 12:21:49 -0500

    Complete form with request

---
+++ Day 60 HTMLA Form Revision - Creating a Form from Scratch/html-forms/main.py
@@ -0,0 +1,19 @@
+from flask import Flask, render_template, request
+
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+
+@app.route("/login", methods=["POST"])
+def receive_data():
+    if request.method == "POST":
+        return f"<h1>Name: {request.form['name']}, Password: {request.form['password']}"
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
