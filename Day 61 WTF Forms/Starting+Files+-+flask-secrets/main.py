d280b6bb729806818b6a78add80b99ff7b8a5dfe
John Davis <512jay@gmail.com>
2023-02-03 13:52:45 -0500

    Receive form data with wtforms

--- Day 61 WTF Forms/Starting+Files+-+flask-secrets/main.py
+++ Day 61 WTF Forms/Starting+Files+-+flask-secrets/main.py
@@ -5,9 +5,8 @@ from wtforms.validators import DataRequired, Email, Length
 
 
 class MyForm(FlaskForm):
-    email = EmailField(label='Email', validators=[DataRequired(), Email( # message="Something is wrong!",
-                                                                        granular_message=True,
-                                                                        check_deliverability=True)])
+    email = EmailField(label='Email',
+                       validators=[DataRequired(), Email(granular_message=True,check_deliverability=True)])
     password = PasswordField(label='Password', validators=[DataRequired(), Length(min=8)])
     recaptcha = RecaptchaField()
     submit = SubmitField(label='Log In')
@@ -26,8 +25,16 @@ def home():
 @app.route("/login", methods=['GET', 'POST'])
 def login():
     login_form = MyForm()
-    login_form.validate_on_submit()
+
+    if login_form.validate_on_submit():
+        if login_form.email.data == "admin@email.com":
+            if login_form.password.data == "12345678":
+                return render_template('success.html')
+
+    if login_form.validate_on_submit():
+        return render_template('denied.html')
     return render_template('login.html', form=login_form)
+
 
 
 if __name__ == '__main__':
