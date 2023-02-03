629406f16d66d77c000dc4df4c88f998d376779a
John Davis <512jay@gmail.com>
2023-02-03 12:58:02 -0500

    Adding Validation to Forms with Flask-WTF

--- Day 61 WTF Forms/Starting+Files+-+flask-secrets/main.py
+++ Day 61 WTF Forms/Starting+Files+-+flask-secrets/main.py
@@ -1,12 +1,14 @@
 from flask import Flask, render_template
 from flask_wtf import FlaskForm, RecaptchaField
-from wtforms import StringField, PasswordField, SubmitField
-from wtforms.validators import DataRequired
+from wtforms import PasswordField, SubmitField, EmailField
+from wtforms.validators import DataRequired, Email, Length
 
 
 class MyForm(FlaskForm):
-    name = StringField(label='Email', validators=[DataRequired()])
-    password = PasswordField(label='Password', validators=[DataRequired()])
+    email = EmailField(label='Email', validators=[DataRequired(), Email( # message="Something is wrong!",
+                                                                        granular_message=True,
+                                                                        check_deliverability=True)])
+    password = PasswordField(label='Password', validators=[DataRequired(), Length(min=8)])
     recaptcha = RecaptchaField()
     submit = SubmitField(label='Log In')
 
@@ -24,6 +26,7 @@ def home():
 @app.route("/login", methods=['GET', 'POST'])
 def login():
     login_form = MyForm()
+    login_form.validate_on_submit()
     return render_template('login.html', form=login_form)
 
 
