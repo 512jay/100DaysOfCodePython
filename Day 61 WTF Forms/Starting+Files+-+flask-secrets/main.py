9f220785cf05a93d8e825e10dc3795ec9dfa3c6f
John Davis <512jay@gmail.com>
2023-02-03 11:57:48 -0500

    Code Improvements for Our WTForms

--- Day 61 WTF Forms/Starting+Files+-+flask-secrets/main.py
+++ Day 61 WTF Forms/Starting+Files+-+flask-secrets/main.py
@@ -1,13 +1,14 @@
 from flask import Flask, render_template
 from flask_wtf import FlaskForm, RecaptchaField
-from wtforms import StringField
+from wtforms import StringField, PasswordField, SubmitField
 from wtforms.validators import DataRequired
 
 
 class MyForm(FlaskForm):
-    name = StringField('name', validators=[DataRequired()])
-    password = StringField('password', validators=[DataRequired()])
+    name = StringField(label='Email', validators=[DataRequired()])
+    password = PasswordField(label='Password', validators=[DataRequired()])
     recaptcha = RecaptchaField()
+    submit = SubmitField(label='Log In')
 
 
 app = Flask(__name__)
