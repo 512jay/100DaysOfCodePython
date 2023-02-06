ed9f9a98c6f4b12dfb72c30a7812da1a35cc5a19
John Davis <512jay@gmail.com>
2023-02-05 19:57:42 -0500

    Complete personal library

--- Day 63 Databases SQLite SQLAlchemy/sqlite3db/main.py
+++ Day 63 Databases SQLite SQLAlchemy/sqlite3db/main.py
@@ -31,6 +31,32 @@ with app.app_context():
     db.session.add(new_book)
     db.session.commit()
 
+# Read All Records
+# all_books = session.query(Book).all()
+#
+#
+# Read A Particular Record By Query
+# book = Book.query.filter_by(title="Harry Potter").first()
+#
+#
+# Update A Particular Record By Query
+# book_to_update = Book.query.filter_by(title="Harry Potter").first()
+# book_to_update.title = "Harry Potter and the Chamber of Secrets"
+# db.session.commit()
+#
+#
+# Update A Record By PRIMARY KEY
+# book_id = 1
+# book_to_update = Book.query.get(book_id)
+# book_to_update.title = "Harry Potter and the Goblet of Fire"
+# db.session.commit()
+#
+#
+# Delete A Particular Record By PRIMARY KEY
+# book_id = 1
+# book_to_delete = Book.query.get(book_id)
+# db.session.delete(book_to_delete)
+# db.session.commit()
 
 # ### Using SQLite database directly
 # import sqlite3
