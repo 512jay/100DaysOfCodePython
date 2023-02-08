64980cd544eb92246e7ac61d828520822427dc6e
John Davis <512jay@gmail.com>
2023-02-08 15:55:53 -0500

    Display all the cafés at the all route.

--- Day_66_Build_a_RESTFUL_API/Cafe_API/main.py
+++ Day_66_Build_a_RESTFUL_API/Cafe_API/main.py
@@ -29,9 +29,30 @@ class Cafe(db.Model):
         return f"{self.name}"
 
     def as_dict(self):
-        return {c.name: getattr(self, c.name) for c in self.__table__.columns}
+        return {column.name: getattr(self, column.name) for column in self.__table__.columns}
+
+    def to_dict(self):
+        # Method 1.
+        dictionary = {}
+        # Loop through each column in the data record
+        for column in self.__table__.columns:
+            # Create a new dictionary entry;
+            # where the key is the name of the column
+            # and the value is the value of the column
+            dictionary[column.name] = getattr(self, column.name)
+        return dictionary
+
+        # Method 2. Alternatively use Dictionary Comprehension to do the same thing.
+        # return {column.name: getattr(self, column.name) for column in self.__table__.columns}
 
 
+# HTTP GET - Read Record
+
+# HTTP POST - Create Record
+
+# HTTP PUT/PATCH - Update Record
+
+# HTTP DELETE - Delete Record
 @app.route("/")
 def home():
     return render_template("index.html")
@@ -41,7 +62,6 @@ def home():
 def get_random_cafe():
     cafes = db.session.query(Cafe).all()
     random_cafe = random.choice(cafes)
-    print(vars(random_cafe))
     # random_cafe_dict = {
     #     "cafe": {
     #         "can_take_calls": random_cafe.can_take_calls,
@@ -57,21 +77,23 @@ def get_random_cafe():
     #         "seats": random_cafe.seats,
     #     }
     # }
-    cafe_dictionary = dict(random_cafe.__dict__)
-    del cafe_dictionary["_sa_instance_state"]
-    return jsonify(cafe=cafe_dictionary)
+    # cafe_dictionary = dict(random_cafe.__dict__)
+    # del cafe_dictionary["_sa_instance_state"]
+    return jsonify(cafe=random_cafe.to_dict())
 
     # return jsonify(random_cafe_dict)
     # return render_template("random.html", cafe=random_cafe)
 
 
-# HTTP GET - Read Record
-
-# HTTP POST - Create Record
-
-# HTTP PUT/PATCH - Update Record
-
-# HTTP DELETE - Delete Record
+@app.route("/all")
+def get_all_cafes():
+    cafes = db.session.query(Cafe).all()
+    # cafes_dict = []
+    # for cafe in cafes:
+    #     cafes_dict.append(cafe.to_dict())
+    # return jsonify(cafes=cafes_dict)
+    # Here is a good example of a list comprehension replacing a loop.
+    return jsonify(cafes=[cafe.to_dict() for cafe in cafes])
 
 
 if __name__ == '__main__':
