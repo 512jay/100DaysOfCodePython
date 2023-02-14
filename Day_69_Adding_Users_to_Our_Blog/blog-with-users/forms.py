1ea93e265b5907f9c75ab8446da9499bc9fad2c4
John Davis <512jay@gmail.com>
2023-02-14 16:03:30 -0500

    Add RegisterForm

--- Day_69_Adding_Users_to_Our_Blog/blog-with-users/forms.py
+++ Day_69_Adding_Users_to_Our_Blog/blog-with-users/forms.py
@@ -3,10 +3,18 @@ from wtforms import StringField, SubmitField, PasswordField
 from wtforms.validators import DataRequired, URL
 from flask_ckeditor import CKEditorField
 
-##WTForm
+
+# WTForm
 class CreatePostForm(FlaskForm):
     title = StringField("Blog Post Title", validators=[DataRequired()])
     subtitle = StringField("Subtitle", validators=[DataRequired()])
     img_url = StringField("Blog Image URL", validators=[DataRequired(), URL()])
     body = CKEditorField("Blog Content", validators=[DataRequired()])
     submit = SubmitField("Submit Post")
+
+
+class RegisterForm(FlaskForm):
+    name = StringField("Name", validators=[DataRequired()])
+    email = StringField("Email", validators=[DataRequired()])
+    password = PasswordField("Password", validators=[DataRequired()])
+
